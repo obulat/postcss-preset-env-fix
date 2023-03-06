1b001414f803d77c0ab57c6833e2ae0cb7d673eb
obulat <obulat@gmail.com>
2023-03-06 15:14:38 +0300

    2 files added, 3 deleted

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex p-5 bg-black">
+    <p class="bg-yellow ps-5 pe-0">
+      <span>20 </span>
+      <span>{{ $t('word') }}</span
+      ><span> 0</span>
+    </p>
+  </div>
+
+</template>
